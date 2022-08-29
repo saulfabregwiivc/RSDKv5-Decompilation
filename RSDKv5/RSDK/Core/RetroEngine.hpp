b8f5bd3af343496f5545456918084c115a846339
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2022-08-29 18:43:11 +0200

    wii: Use stdint for platform agnostic type sizes

--- RSDKv5/RSDK/Core/RetroEngine.hpp
+++ RSDKv5/RSDK/Core/RetroEngine.hpp
@@ -12,6 +12,17 @@
 // ================
 // STANDARD TYPES
 // ================
+#ifdef __wii__
+#include <cstdint>
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
+#else
 typedef signed char int8;
 typedef unsigned char uint8;
 typedef signed short int16;
@@ -20,7 +31,7 @@ typedef signed int int32;
 typedef unsigned int uint32;
 typedef signed long long int64;
 typedef unsigned long long uint64;
-
+#endif
 typedef uint32 bool32;
 typedef uint32 color;
 
