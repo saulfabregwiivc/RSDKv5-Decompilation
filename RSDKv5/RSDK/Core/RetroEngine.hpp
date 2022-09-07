09bec21ba6dfce15a735396298305e6812eab281
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2022-09-07 22:46:02 +0200

    wii: Implement Wiimote input

--- RSDKv5/RSDK/Core/RetroEngine.hpp
+++ RSDKv5/RSDK/Core/RetroEngine.hpp
@@ -434,6 +434,7 @@ enum GameRegions {
 #define RETRO_AUDIODEVICE_WII (1)
 #undef RETRO_INPUTDEVICE_WII
 #define RETRO_INPUTDEVICE_WII (1)
+#undef RETRO_INPUTDEVICE_KEYBOARD
 
 #endif
 
--- RSDKv5/RSDK/Input/Wii/WiiInputDevice.hpp
+++ RSDKv5/RSDK/Input/Wii/WiiInputDevice.hpp
@@ -5,6 +5,21 @@ struct InputDeviceWii : InputDevice {
     void UpdateInput();
     void ProcessInput(int32 controllerID);
     void CloseDevice();
+
+    uint32 buttonMasks;
+    uint32 prevButtonMasks;
+    uint8 stateUp;
+    uint8 stateDown;
+    uint8 stateLeft;
+    uint8 stateRight;
+    uint8 stateA;
+    uint8 stateB;
+    uint8 stateC;
+    uint8 stateX;
+    uint8 stateY;
+    uint8 stateZ;
+    uint8 stateStart;
+    uint8 stateSelect;
 };
 
 void InitWiiInputAPI();
