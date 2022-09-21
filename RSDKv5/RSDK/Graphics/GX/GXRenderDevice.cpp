7a2582341adcca5fab28e06be275456d6cf1064a
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2022-09-21 18:18:29 +0200

    wii: GX Init

--- RSDKv5/RSDK/Graphics/GX/GXRenderDevice.cpp
+++ RSDKv5/RSDK/Graphics/GX/GXRenderDevice.cpp
@@ -1,4 +1,23 @@
+#include <gccore.h>
+
+static void *xfb;
+static GXRModeObj *rmode;
+
 bool RenderDevice::Init() {
+    VIDEO_Init();
+
+    rmode = VIDEO_GetPreferredMode(NULL);
+
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(xfb);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+    if (rmode->viTVMode & VI_NON_INTERLACE)
+        VIDEO_WaitVSync();
     return true;
 }
 
