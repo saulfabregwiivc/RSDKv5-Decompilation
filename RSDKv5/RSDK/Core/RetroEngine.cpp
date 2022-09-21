9721cf03cbcdc17357301bfbbc5ad7da8b8ef774
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2022-09-21 18:17:05 +0200

    wii: Init FAT for SD card reading

--- RSDKv5/RSDK/Core/RetroEngine.cpp
+++ RSDKv5/RSDK/Core/RetroEngine.cpp
@@ -1406,6 +1406,14 @@ static void initNxLink()
 }
 #endif
 
+#if __wii__
+#include <debug.h>
+#include <ogcsys.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <fat.h>
+#endif
+
 void RSDK::InitCoreAPI()
 {
 #if RETRO_RENDERDEVICE_DIRECTX9 || RETRO_RENDERDEVICE_DIRECTX11
@@ -1418,6 +1426,13 @@ void RSDK::InitCoreAPI()
     Result res;
     if (R_FAILED(res = dynInitialize()))
         diagAbortWithResult(res);
+#endif
+
+#if __wii__
+    if (!fatInitDefault()) {
+        exit(1);
+    }
+    DEBUG_Init(GDBSTUB_DEVICE_USB, 1);
 #endif
 
 #if RETRO_RENDERDEVICE_SDL2 || RETRO_AUDIODEVICE_SDL2 || RETRO_INPUTDEVICE_SDL2
